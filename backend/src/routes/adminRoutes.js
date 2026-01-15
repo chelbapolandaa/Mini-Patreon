2592929207ee7d6fe4bd4cbc3684fa8626e6f3df
chelbapolandaa <kesyuno7@gmail.com>
2026-01-15 17:56:21 +0700

    More Robust on Admin Routes

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const { protect, authorize } = require('../middleware/authMiddleware');
 const { getAllUsers, banUser, getAllTransactions } = require('../controllers/adminController');
 
-// All admin routes require admin role
 router.use(protect);
 router.use(authorize('admin'));
 
